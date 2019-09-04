fda5fe36430149bd5f67e4cda5263b1c24092863
aleksandr-staetskiy <aleksandrstaetskiy@gmail.com>
2019-09-04 17:11:56 +0300

    desc text added to promo section

--- gulp/tasks/img.js
+++ gulp/tasks/img.js
@@ -3,9 +3,6 @@ module.exports = function() {
     return $.gulp.src('src/static/img/*.{png,jpg,svg}')
       // .pipe($.gp.tinypng('dLgHNpGZ0yRW788WMHGwfBnxr0DlLHD9'))
       .pipe($.gulp.dest('build/static/img/'));
-});
-<<<<<<< HEAD
+  });
 } 
-=======
-}
->>>>>>> c4e1b54798bd8b691ad47b94f6eb74a7b1c77a96
+
--- gulpfile.js
+++ gulpfile.js
@@ -16,11 +16,8 @@ $.path.tasks.forEach(function (taskPath){
 })
 
 $.gulp.task('default', $.gulp.series(
-<<<<<<< HEAD
   $.gulp.parallel('pug', 'sass', 'scripts:lib', 'scripts',  'img', 'fonts'),
-=======
   $.gulp.parallel('pug', 'sass', 'scripts:lib', 'scripts', 'img'),
->>>>>>> c4e1b54798bd8b691ad47b94f6eb74a7b1c77a96
   $.gulp.parallel('watch', 'serve')
 ));
 
