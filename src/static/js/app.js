886f0d85adce04565722293eb222c9c35ee9ec39
aleksandr-staetskiy <aleksandrstaetskiy@gmail.com>
2019-09-15 17:32:46 +0300

    courses section view

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,3 +1,27 @@
 $(document).ready(function () {
-  $('.slider').slick();
+  $('.slider').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
